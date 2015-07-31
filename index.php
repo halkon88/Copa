5811005120d5d97e8c13026c534cc36248ec80f4
Lino Ramirez <hastalavictoriasiempre@hotmail.com>
2015-07-31 15:58:51 -0600

    Se ha creado el archivo index

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+<head><title>Torneo Nacional del Pacifico de Futbol</title></head>
+<body>
+<h1>Este es el sitio en construccion del torneo nacional del pacifico de futbol</h1>
+</body>
+</html>
